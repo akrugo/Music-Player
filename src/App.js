9f406f01a80a9d58e44f44db4e8ab8d9e63d4c08
WADIE Hamza <adie.hamzaa@gmail.com>
2021-04-21 19:02:28 +0000

    git remote add origin https://github.com/akrugo/Music-Player.git git branch -M main git push -u origin main

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Player from "./Player";
+import Song from "./Song";
+
+function App() {
+  return (
+    <div>
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Player.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
